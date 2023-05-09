f13812598f9c3e291aa5456b5e18a3becbf81d89
Vol-Power <victor.moor@live.com>
2023-05-09 20:51:47 -0300

    adicionando projeto no git

---
+++ Pensionista/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pensionista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos quartos vão ser alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Quartos[] vet = new Quartos[10];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Reserva #"+(i+1));
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vet[quarto] = new Quartos(nome, email, quarto);
+            }
+
+            Console.WriteLine("Quartos alugados");
+            for (int i = 0;i < 10; i++)
+            {
+                if (vet[i] != null )
+                {
+                    Console.WriteLine(vet[i].Quarto + ": " + vet[i].Nome + ", " + vet[i].Email);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Pensionista/Quartos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pensionista
+{
+    internal class Quartos
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set;}
+
+        public Quartos(string nome, string email, int quarto)
+        {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+    }
+}
